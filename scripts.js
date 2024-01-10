367dd734c8602391acff87c579254b6d643df728
samflwr <109246278+samflwr@users.noreply.github.com>
2024-01-10 18:19:37 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuIcon.addEventListener('click', function() {
+        navMenu.classList.toggle('open');
+    });
+});
