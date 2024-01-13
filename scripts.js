cbf5a70da28c536ee79c93847c256a42ce331073
samflwr <109246278+samflwr@users.noreply.github.com>
2024-01-13 23:00:32 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navMenu = document.querySelector('.nav-menu');
-
-    menuIcon.addEventListener('click', function() {
-        navMenu.classList.toggle('open');
-    });
-});
